fd72e7f6a7b9c9c344a4bf3b3256a27c3b6549df
valerimagic <49991533+valerimagic@users.noreply.github.com>
2019-04-26 12:19:13 +0300

    zadacha

---
+++ Array/01.Train/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _01.Train
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int[] wagon = new int[number];
+
+            for (int i = 0; i < number; i++)
+            {
+                wagon[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(string.Join(" ", wagon));
+            Console.WriteLine(wagon.Sum());
+        }
+    }
+}
