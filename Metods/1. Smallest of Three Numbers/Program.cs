f40cacf072ec5229ef5ed6261580c6f503bb2fbf
valerimagic <49991533+valerimagic@users.noreply.github.com>
2019-10-31 22:34:04 +0200

    new files

---
+++ Metods/1. Smallest of Three Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Smallest_of_Three_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[3];
+            for (int i = 0; i < 3; i++)
+            {
+            num[i] = int.Parse(Console.ReadLine());
+
+            }
+            int smallNumber = SmallerNumber(num);
+            Console.WriteLine(smallNumber);
+        }
+
+        static int SmallerNumber(int[] numbers)
+        {
+           
+            int result = numbers.Min();
+            return result;
+           
+
+        }
+    }
+}
