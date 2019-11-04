066ea2a7ef17ad3e13429a023e421de560382cab
valerimagic <49991533+valerimagic@users.noreply.github.com>
2019-11-04 21:52:56 +0200

    new files

---
+++ Array/2. Vowels Count/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _2._Vowels_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().ToLower();
+            Vowel(input);
+        }
+
+        static void Vowel(string word)
+        {
+            int count = 0;
+
+            foreach (var item in word)
+            {
+                if(item == 'a' || item == 'e' || item =='o' || item== 'u' 
+                    || item == 'i' || item == 'y')
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
