77d8e7781345273dbe7f9df8f68a2137048e28ef
valerimagic <49991533+valerimagic@users.noreply.github.com>
2019-11-05 18:41:35 +0200

    new

---
+++ Array/9.Palindrome_Integers/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _9.Palindrome_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var input = Console.ReadLine();
+            var left = new List<char>();
+            var rigth = new List<char>();
+
+            while(input != "END")
+            {
+                
+
+                for (int i = 0; i < input.Length/2; i++)
+                {
+                    left.Add(input[i]);
+                }
+
+                for (int i = input.Length-1; i > input.Length/2; i--)
+                {
+                    rigth.Add(input[i]);
+                }
+
+
+                //var revers = Reverse(input);
+                //for (int i = 0; i < input.Length/2; i++)
+                //{
+                //    rigth.Add(revers[i]);
+                //}
+
+
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Dictionaries/1,UniqueUsernames/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1_UniqueUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            HashSet<string> names = new HashSet<string>();
+
+            for (int i = 0; i < num; i++)
+            {
+                string input = Console.ReadLine();
+                names.Add(input);
+            }
+
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
