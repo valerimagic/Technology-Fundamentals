24ebeede84741b0e3c658e6d368305f03de40cf3
valerimagic <49991533+valerimagic@users.noreply.github.com>
2019-11-05 18:44:00 +0200

    new

---
+++ Dictionaries/2.SetsofElements/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2.SetsofElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> first = new HashSet<int>();
+            HashSet<int> second = new HashSet<int>();
+
+            var num = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < num[0]; i++)
+            {
+                var input = int.Parse(Console.ReadLine());
+                first.Add(input);
+            }
+            for (int i = 0; i < num[1]; i++)
+            {
+                var input = int.Parse(Console.ReadLine());
+                second.Add(input);
+            }
+            if (first.Count > second.Count)
+            {
+
+                foreach (var item in second)
+                {
+                    if (first.Contains(item))
+                    {
+                        Console.Write(item + " ");
+                    }
+
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                foreach (var item in first)
+                {
+                    if (second.Contains(item))
+                    {
+                        Console.Write(item + " ");
+                    }
+
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
