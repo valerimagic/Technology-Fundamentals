041b49eec90ddf31adee693f0861974826a83e8c
valerimagic <49991533+valerimagic@users.noreply.github.com>
2019-11-06 19:01:40 +0200

    6.11.19

---
+++ Dictionaries/3,PeriodicTable/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3_PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = int.Parse(Console.ReadLine());
+
+            var set = new SortedSet<string>();
+
+            for (int i = 0; i < num; i++)
+            {
+                var input = Console.ReadLine().Split();
+                foreach (var item in input)
+                {
+                    set.Add(item);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", set));
+        }
+    }
+}
