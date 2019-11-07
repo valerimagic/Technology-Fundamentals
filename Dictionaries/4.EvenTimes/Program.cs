58c955f68dc0dc7413ead95b17ed148e54d85909
valerimagic <49991533+valerimagic@users.noreply.github.com>
2019-11-07 18:49:23 +0200

    07.11.2019

---
+++ Dictionaries/4.EvenTimes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4.EvenTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            var dict = new Dictionary<int, int>();
+
+            for (int i = 0; i < num; i++)
+            {
+                var input = int.Parse(Console.ReadLine());
+
+                if (!dict.ContainsKey(input))
+                {
+                    dict.Add(input, 1);
+                }
+                else
+                {
+                    dict[input]++;
+                }
+            }
+
+            Console.WriteLine(dict.First(x => x.Value % 2 == 0).Key);
+        }
+    }
+}
