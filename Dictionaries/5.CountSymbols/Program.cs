54ad7c7d969b34b616fe1735737b18b6aebffb0c
valerimagic <49991533+valerimagic@users.noreply.github.com>
2019-11-09 12:16:41 +0200

    new

---
+++ Dictionaries/5.CountSymbols/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5.CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().ToArray();
+
+            var dict = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+
+           
+                if (!dict.ContainsKey(input[i]))
+                {
+                    dict.Add(input[i], 0);
+                }
+                dict[input[i]]++;
+            }
+
+            foreach (var item in dict)
+            {
+
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+                
+            }
+        }
+    }
+}
