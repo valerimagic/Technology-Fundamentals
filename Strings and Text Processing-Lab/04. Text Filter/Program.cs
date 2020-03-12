53c28a9cb58542e198700b621514c206c6530328
valerimagic <49991533+valerimagic@users.noreply.github.com>
2020-03-12 22:13:45 +0200

    new

---
+++ Strings and Text Processing-Lab/04. Text Filter/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _04._Text_Filter
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+            List<string> banned = Console.ReadLine()
+                                  .Split(", ")
+                                  .ToList();
+
+            string text = Console.ReadLine();
+
+            foreach (var word in banned)
+            {
+                text = text.Replace(word, new string('*', word.Length));
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
