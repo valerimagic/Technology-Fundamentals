2b335dfd01367dd3e5fe38a69147b6a6acac72b8
valerimagic <49991533+valerimagic@users.noreply.github.com>
2020-03-15 15:20:04 +0200

    new

---
+++ Strings and Text Processing-Lab/05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _05._Digits__Letters_and_Other
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main()
+        {
+            Regex digitRegex = new Regex(@"\d");
+            Regex charRegex = new Regex(@"[A-Za-z]");
+            Regex anyOtherRegex = new Regex(@"[\W|_]");
+
+            string input = Console.ReadLine();
+
+            string digits = string.Join(string.Empty, digitRegex.Matches(input));
+            string chars = string.Join(string.Empty, charRegex.Matches(input));
+            string others = string.Join(string.Empty, anyOtherRegex.Matches(input));
+
+            Console.WriteLine(digits);
+            Console.WriteLine(chars);
+            Console.WriteLine(others);
+                        
+        }
+    }
+}
